c618a24f5d24115bfe3215556e7ae6a73556708f
Dave Elcock <dave.git.2012@coolhandmook.com>
2012-02-26 11:13:13 +0000

    Moved trayjenins tests into trayjenkins folder.

--- runAllTests.py
+++ runAllTests.py
@@ -6,10 +6,10 @@ import os
 
 sys.path.append('submodules/pyjenkins')
 
-from tests.EventTests import EventTests
+from tests.trayjenkins.EventTests import EventTests
 
-from tests.test_jobs import *
-from tests.test_status import *
+from tests.trayjenkins.test_jobs import *
+from tests.trayjenkins.test_status import *
 
 if __name__ == '__main__':
     unittest.main(testRunner= unittest.TextTestRunner(stream= sys.stdout, verbosity=2))
