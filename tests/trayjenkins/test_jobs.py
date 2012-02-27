9b78c20b8b5b240c0bd5d873210497e124343df9
Dave E <dave.git.2012@coolhandmook.com>
2012-02-27 13:46:13 +0000

    Refactor model tests to use setUp()

--- tests/trayjenkins/test_jobs.py
+++ tests/trayjenkins/test_jobs.py
@@ -32,86 +32,67 @@ class JobsPresenterTests(TestCase):
 
 class JobsModelTests(TestCase):
 
+    def setUp(self):
+        
+        self.mocks = mox.Mox()
+        self.jenkins = self.mocks.CreateMock(Jenkins)
+        self.factory = self.mocks.CreateMock(IJenkinsFactory)
+        self.event = self.mocks.CreateMock(IEvent)
+        self.server = Server('host', 'uname', 'pw')
+
     def test_updateJobs_FirstCall_FireJobsUpdatedEventWithRetrievedJobs(self):
 
-        mocks = mox.Mox()
-        jenkins = mocks.CreateMock(Jenkins)
-        factory = mocks.CreateMock(IJenkinsFactory)
-        event = mocks.CreateMock(IEvent)
-
-        server = Server('host', 'uname', 'pw')
         jobs = [Job('job1', JobStatus.OK), Job('job2', JobStatus.FAILING)]
-        factory.create(server).AndReturn(jenkins)
-        jenkins.list_jobs().AndReturn(jobs)
-        event.fire(jobs)
+        self.factory.create(self.server).AndReturn(self.jenkins)
+        self.jenkins.list_jobs().AndReturn(jobs)
+        self.event.fire(jobs)
+        self.mocks.ReplayAll()
 
-        mocks.ReplayAll()
-
-        model = Model(server, factory, event)
+        model = Model(self.server, self.factory, self.event)
         model.updateJobs()
 
-        mox.Verify(event)
+        mox.Verify(self.event)
 
     def test_updateJobs_SecondCallReturnsSameJobs_JobsUpdatedEventNotFiredOnceOnly(self):
 
-        mocks = mox.Mox()
-        jenkins = mocks.CreateMock(Jenkins)
-        factory = mocks.CreateMock(IJenkinsFactory)
-        event = mocks.CreateMock(IEvent)
-
-        server = Server('host', 'uname', 'pw')
         jobs = [Job('job1', JobStatus.OK), Job('job2', JobStatus.FAILING)]
-        factory.create(server).AndReturn(jenkins)
-        jenkins.list_jobs().AndReturn(jobs)
-        jenkins.list_jobs().AndReturn(jobs)
-        event.fire(jobs)
+        self.factory.create(self.server).AndReturn(self.jenkins)
+        self.jenkins.list_jobs().AndReturn(jobs)
+        self.jenkins.list_jobs().AndReturn(jobs)
+        self.event.fire(jobs)
+        self.mocks.ReplayAll()
 
-        mocks.ReplayAll()
-
-        model = Model(server, factory, event)
+        model = Model(self.server, self.factory, self.event)
         model.updateJobs()
         model.updateJobs()
 
-        mox.Verify(event)
+        mox.Verify(self.event)
 
     def test_updateJobs_SecondCallReturnsDifferentJobs_JobsUpdatedEventFiredForEachResult(self):
 
-        mocks = mox.Mox()
-        jenkins = mocks.CreateMock(Jenkins)
-        factory = mocks.CreateMock(IJenkinsFactory)
-        event = mocks.CreateMock(IEvent)
-
-        server = Server('host', 'uname', 'pw')
         jobsOne = [Job('job1', JobStatus.OK), Job('job2', JobStatus.FAILING)]
         jobsTwo = [Job('job1', JobStatus.OK), Job('job2', JobStatus.OK)]
-        factory.create(server).AndReturn(jenkins)
-        jenkins.list_jobs().AndReturn(jobsOne)
-        jenkins.list_jobs().AndReturn(jobsTwo)
-        event.fire(jobsOne)
-        event.fire(jobsTwo)
+        self.factory.create(self.server).AndReturn(self.jenkins)
+        self.jenkins.list_jobs().AndReturn(jobsOne)
+        self.jenkins.list_jobs().AndReturn(jobsTwo)
+        self.event.fire(jobsOne)
+        self.event.fire(jobsTwo)
+        self.mocks.ReplayAll()
 
-        mocks.ReplayAll()
-
-        model = Model(server, factory, event)
+        model = Model(self.server, self.factory, self.event)
         model.updateJobs()
         model.updateJobs()
 
-        mox.Verify(event)
+        mox.Verify(self.event)
 
     def test_jobsUpdatedEvent_ReturnsEventFromConstructor(self):
 
-        mocks = mox.Mox()
-        factory = mocks.CreateMock(IJenkinsFactory)
-        event = mocks.CreateMock(IEvent)
+        self.factory.create(mox.IgnoreArg()).AndReturn(None)
+        self.mocks.ReplayAll()
 
-        server = Server('dunt', 'really', 'matter')
-        factory.create(mox.IgnoreArg()).AndReturn(None)
+        model = Model(self.server, self.factory, self.event)
 
-        mocks.ReplayAll()
-
-        model = Model(server, factory, event)
-
-        self.assertTrue(event is model.jobsUpdatedEvent())
+        self.assertTrue(self.event is model.jobsUpdatedEvent())
 
 
 class NoFilterTests(TestCase):
